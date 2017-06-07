446f4521b2f2b019a555b4548429823f1abbe19c
deadzer0 <gendarmery@gmail.com>
2017-06-07 14:13:55 +0300

    Update mailer.js

--- config/mailer.js
+++ config/mailer.js
@@ -5,8 +5,8 @@ module.exports = {
         'host': 'debugmail.io',
         'port': '25',
         'auth': {
-            'user': 'gendarmery@abv.bg',
-            'pass': 'de5f73c0-4505-11e7-b8ac-59cc35386e58'
+            'user': 'xxxxxxxxxxxxx',
+            'pass': 'xxxxxxxxxxxxx'
         }
     }
 };
