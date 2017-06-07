4c6920b2ff5c55047f7375d5224908ae7ddf9c68
deadzer0 <gendarmery@gmail.com>
2017-06-07 14:12:34 +0300

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -2,13 +2,13 @@
 
 module.exports = {
   'facebookAuth': {
-      'clientID': '132394083994278',
-      'clientSecret': '83b7f9ad60b17ca792c0ce6b7fdc8636',
+      'clientID': 'xxxxxxxx',
+      'clientSecret': 'xxxxxxxx',
       'callbackURL': 'http://localhost:3001/auth/facebook/callback'
   },
     'googleAuth': {
-        'clientID': '213679349634-jm89rqlabhsl4fcpvjqjhjol7283erui.apps.googleusercontent.com',
-        'clientSecret': 'Uc6AFroHtpYuksb_CmYFrUSB',
+        'clientID': 'xxxxxxxxx',
+        'clientSecret': 'xxxxxxxxx',
         'callbackURL': 'http://localhost:3001/auth/google/callback'
     }
 };
